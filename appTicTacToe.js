8e49c6007604faa01a02401acbed59b1599010fd
Sylvie-B <unsolo@hotmail.fr>
2020-12-09 21:13:39 +0100

    js append child

--- appTicTacToe.js
+++ appTicTacToe.js
@@ -1,14 +1,29 @@
-document.addEventListener("contextmenu", function (event){
+
+let board = document.getElementById("ticTacToe");
+board.addEventListener("contextmenu", function (event){
     event.preventDefault();
 })
 
-let board = document.getElementById("ticTacToe");
-let square = board.getElementsByTagName("div");
+let square = board.getElementsByTagName("div");   // get squares
+let cross = document.createElement("img");          // create img player 1
+cross.style.width = "9vw";
+cross.style.height = "9vh";
+cross.src = "croix.png";
+
+let round = document.createElement("img");          // create img player 2
+round.style.width = "9vw";
+round.style.height = "9vh";
+round.src = "buttonGreen.png";
+
+// square[0].appendChild(cross);
+// square[1].appendChild(round);
 
 console.log(square);
 
 for (let i = 0; i < square.length; i++){
-    square[i].addEventListener("pointerup", function (event){
+    square[i].addEventListener("mouseup", function (event){
         console.log(event.button);
-    }, false)
+        square[i].appendChild(cross);
+    })
 }
+
