1883f02017b7a0d32fb0f84964636dbcdd660836
Sylvie-B <unsolo@hotmail.fr>
2020-12-10 16:15:42 +0100

    restart

--- appTicTacToe.js
+++ appTicTacToe.js
@@ -8,29 +8,29 @@ let board = document.getElementsByClassName("case");
 
 const playerX = 0;
 const playerO = 2;
+
 let turn = playerX;
 let info = document.getElementById("info");
-let score1 = "";
-let score2 = "";
-let pNum = document.getElementsByClassName("pNum");
-console.log(pNum.length);
 
+let pNum = document.getElementsByClassName("pNum");
 
 for (let square of board){
     square.addEventListener("mouseup", function (event){
 
         switch (event.button){
             case 0:
-                if (turn === 0){
+                if (turn === event.button){
                     isItEmpty(this, "<img alt='O' src=\'croix.png\'>");
                     turn = playerO;
+                    info.innerHTML = "Joueur 2";
                 }
 
                 break;
             case 2:
-                if (turn === 2){
+                if (turn === event.button){
                     isItEmpty(this, "<img alt='O' src=\'buttonGreen.png\'>");
                     turn = playerX;
+                    info.innerHTML = "Joueur 1"
                 }
                 break;
         }
